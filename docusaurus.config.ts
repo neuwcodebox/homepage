d41bb25e90faac27a4cf7ad582d7d67b82f1e857
Neuwcodebox <neuwgmbox@gmail.com>
2026-07-25 23:22:29 +0900

    fix: update truncate comment syntax to %%TRUNCATE%% in blog and config files

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -47,7 +47,7 @@ const config: Config = {
         },
         blog: {
           showReadingTime: true,
-          truncateMarker: /^<<<\s*truncate\s*>>>$/m,
+          truncateMarker: /^%%TRUNCATE%%$/m,
           feedOptions: {
             type: ['rss', 'atom'],
             xslt: true,
